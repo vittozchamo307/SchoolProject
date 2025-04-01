e1a281ea1cafd8fcbe266146c7393670e1f5b9b5
vittozchamo307 <vittozchamo307@gmail.com>
2025-04-01 15:42:11 +0000

    Add 21.php

---
+++ src/21.php
@@ -0,0 +1,9 @@
+<?php
+// Define some variables
+$studentName = "John Doe";
+$studentAge = 20;
+$message = "Hello, $studentName! You are $studentAge years old.";
+
+// Output the message to the user
+echo $message;
+?>

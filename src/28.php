41ca5b60fb701acfb58b768bd8c329ea2b419b05
vittozchamo307 <vittozchamo307@gmail.com>
2025-04-07 05:15:50 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,8 @@
+<?php
+// This PHP script is used to demonstrate the structure and basic usage of GitHub repositories.
+// It includes the "README" file and has an initial commit with a message indicating it's a starting point.
+$repositoryName = "SchoolProject";  // Replace this with your desired repository name
+
+echo "
+* [GitHub Repository](https://github.com/$repositoryName) - This is a school project for learning GitHub."
+?>

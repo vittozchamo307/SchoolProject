da8e8db367aed2007be37f523c27630cd44b7f9b
vittozchamo307 <vittozchamo307@gmail.com>
2025-04-07 06:18:56 +0000

    Add 32.php

---
+++ src/32.php
@@ -0,0 +1,11 @@
+<?php
+$author = "AuthorName";
+$text = <<<EOD
+This is a sample PHP code.
+The author is $author.
+Please check it out and let me know if you find any bugs or issues.
+
+-EOD
+
+echo $text;
+?>

b1d956735adfd87586be8ea8eb8d4354a21e2932
vittozchamo307 <vittozchamo307@gmail.com>
2025-05-23 05:09:25 +0000

    Add 47.php

---
+++ src/47.php
@@ -0,0 +1,10 @@
+<?php
+// This example PHP script demonstrates basic usage and data types.
+// You can use this to store information or perform simple calculations with variables.
+
+// Define variables
+$a = 10;
+$b = 20;
+
+// Print variables
+echo "a: $a, b: $b";
